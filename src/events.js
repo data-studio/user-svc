54e9b07be2eba814a4c1b849bf4cb5221f92019c
Callan Milne <callan.milne@eviratec.com.au>
2017-09-06 11:31:11 +0930

    (fix):Events

--- src/events.js
+++ src/events.js
@@ -16,37 +16,42 @@ module.exports = function (dataStudio) {
   let events = dataStudio.events = new DataStudioEmitter();
 
   events.addListener("auth/attempt:success", function (d) {
-    AuthAttempt.forge({
-      Id: d.Id,
-      Login: d.Login,
-      Finished: true,
-      Error: null,
-      TokenId: d.TokenId,
-      Created: Math.floor(Date.now()/1000),
-    }).save();
+    async(function () {
+      AuthAttempt.forge({
+        Id: d.Id,
+        Login: d.Login,
+        Finished: true,
+        Error: null,
+        TokenId: d.TokenId,
+        Created: Math.floor(Date.now()/1000),
+      }).save();
+    });
   });
 
   events.addListener("auth/attempt:error", function (d) {
-    AuthAttempt.forge({
-      Id: d.Id,
-      Login: d.Login,
-      Finished: true,
-      Error: d.Error,
-      TokenId: null,
-      Created: Math.floor(Date.now()/1000),
-    }).save();
+    async(function () {
+      AuthAttempt.forge({
+        Id: d.Id,
+        Login: d.Login,
+        Finished: true,
+        Error: d.Error,
+        TokenId: null,
+        Created: Math.floor(Date.now()/1000),
+      }).save();
+    });
   });
 
   events.addListener("resource:created", function (uri, createdByUserId) {
-    dataStudio.authz.registerOwnership(uri, createdByUserId);
-    if (uri.split(/\//g).length !== 5) {
-      return;
-    }
-    dataStudio.authz.registerOwnership("/"+uri.split(/\//g).slice(3,2).join("/"), createdByUserId);
+    async(function () {
+      dataStudio.authz.registerOwnership(uri, createdByUserId);
+      if (uri.split(/\//g).length !== 5) {
+        return;
+      }
+      dataStudio.authz.registerOwnership("/"+uri.split(/\//g).slice(3,2).join("/"), createdByUserId);
+    });
   });
 
   events.addListener("signup:success", function (d) {
-
     async(function () {
 
       let user = d[1];
@@ -56,7 +61,6 @@ module.exports = function (dataStudio) {
       console.log(`New user account created <${userLogin}>`);
 
     });
-
   });
 
   function async (f) {
